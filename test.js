f96509d30124cf173309932753ecd6a57a00f027
tunnckoCore <mameto_100@mail.bg>
2015-01-21 05:01:26 +0200

    init commits

---
+++ test.js
@@ -0,0 +1,14 @@
+/**
+ * handle-callback <https://github.com/tunnckoCore/handle-callback>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict';
+
+var handleCallback = require('./index');
+
+describe('handle-callback:', function() {
+  // body
+});
