cfe6695cc90951c72bc6b94db186550cb058644d
tunnckoCore <mameto_100@mail.bg>
2015-06-05 13:51:59 +0300

    refactor - close #1, close #2, close #3

--- index.js
+++ index.js
@@ -1,39 +1,24 @@
-/**
- * handle-callback <https://github.com/tunnckoCore/handle-callback>
+/*!
+ * handle-callback <https://github.com/hybridables/handle-callback>
  *
- * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
  * Released under the MIT license.
  */
 
-'use strict';
+'use strict'
 
-var isPromise = require('is-promise');
+var isPromise = require('is-promise')
+var promise2thunk = require('promise2thunk')
 
-/**
- * Initial step for creating hybrid APIs.
- *
- * @param  {Promise}   `promise`
- * @param  {Function}  `callback`
- * @return {Promise}
- * @api public
- */
-module.exports = function handleCallback(promise, callback) {
+module.exports = function handleCallback (promise, callback) {
   if (!isPromise(promise)) {
-    throw new TypeError('handle-callback: expect `promise` to be Promise');
+    throw new TypeError('handle-callback expect `promise` be Promise')
   }
   if (typeof callback !== 'function') {
-    throw new TypeError('handle-callback: expect `callback` to be function');
+    throw new TypeError('handle-callback expect `callback` be function')
   }
 
-  promise.then(function handleCallback_responseFulfilled(res) {
-    callback(null, res);
-    return res;
-  })
+  promise2thunk(promise)(callback)
 
-  promise.catch(function handleCallback_responseRejected(err) {
-    callback(err);
-    throw err;
-  });
-
-  return promise;
-};
+  return promise
+}
--- test.js
+++ test.js
@@ -1,143 +1,114 @@
-/**
- * handle-callback <https://github.com/tunnckoCore/handle-callback>
+/*!
+ * handle-callback <https://github.com/hybridables/handle-callback>
  *
- * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
  * Released under the MIT license.
  */
 
-'use strict';
+/* jshint asi:true */
 
-var handleCallback = require('./index');
-var thenGot = require('then-got');
-var assert = require('assert');
+'use strict'
 
-function fixture(url, callback) {
-  var promise = thenGot.get(url);
+var test = require('assertit')
+var handleCallback = require('./index')
+var got = require('then-got')
+
+function fixture (url, callback) {
+  var promise = got(url)
+
   if (callback) {
-    promise = handleCallback(promise, callback);
+    promise = handleCallback(promise, callback)
   }
-  return promise;
+
+  return promise
 }
 
-describe('handle-callback:', function() {
-  describe('should work', function() {
-    it('with callback api', function(done) {
-      this.timeout(10000);
+test('handle-callback:', function () {
+  test('should throw TypeError if not a promise given', function (done) {
+    function fixture () {
+      handleCallback(12345)
+    }
 
-      fixture('https://github.com', function(err, res) {
-        var body = res[0];
-        var stream = res[1];
+    test.throws(fixture, TypeError)
+    test.throws(fixture, /handle-callback expect `promise` be Promise/)
+    done()
+  })
+  test('should throw TypeError if not a function given as second param', function (done) {
+    function fixture () {
+      var promise = got('https://github.com')
+      handleCallback(promise, 12345)
+    }
 
-        // callback api
+    test.throws(fixture, TypeError)
+    test.throws(fixture, /handle-callback expect `callback` be function/)
+    done()
+  })
 
-        assert.strictEqual(err, null);
-        assert.ok(res);
-        assert.ok(stream);
-        assert.strictEqual(body[0], '<'); // it is html
-        done();
-      });
-    });
+  test('should work with callback api', function (done) {
+    fixture('https://github.com', function (err, res) {
+      var body = res[0]
 
-    it('with promise api', function(done) {
-      this.timeout(10000);
+      test.ifError(err)
+      test.ok(body.indexOf('<title>') !== -1)
+      done()
+    })
+  })
+  test('should handle errors with callback api', function (done) {
+    fixture('random://github.com', function (err) {
+      test.ifError(!err)
+      test.ok(/Protocol.*not supported/.test(err.message))
+      done()
+    })
+  })
 
-      fixture('https://github.com')
-      .then(function(res) {
-        var body = res[0];
-        var stream = res[1];
+  test('should work with promise api', function (done) {
+    fixture('https://github.com')
+    .then(function (res) {
+      var body = res[0]
 
-        // promise api
+      test.ok(body.indexOf('<title>') !== -1)
+      done()
+    })
+  })
+  test('should catch errors with promise api', function (done) {
+    fixture('random://github.com')
+    .catch(function (err) {
+      test.ifError(!err)
+      test.ok(/Protocol.*not supported/.test(err.message))
+      done()
+    })
+  })
 
-        assert.ok(res);
-        assert.ok(stream);
-        assert.strictEqual(body[0], '<'); // it is html
-        done();
-      });
-    });
+  test('should work both promise and callback api in same time', function (done) {
+    var marker = 11
+    fixture('https://github.com', function (err, res) {
+      var body = res[0]
 
-    it('with both callback and promise api', function(done) {
-      this.timeout(10000);
+      test.ifError(err)
+      test.ok(body.indexOf('<title>') !== -1)
+      marker += 21
+    })
+    .then(function (res) {
+      var body = res[0]
 
-      fixture('https://github.com', function(err, res) {
-        var body = res[0];
-        var stream = res[1];
-
-        // callback api
-
-        assert.strictEqual(err, null);
-        assert.ok(res);
-        assert.ok(stream);
-        assert.strictEqual(body[0], '<'); // it is html
-      })
-      .then(function(res) {
-        var body = res[0];
-        var stream = res[1];
-
-        // promise api
-
-        assert.ok(res);
-        assert.ok(stream);
-        assert.strictEqual(body[0], '<'); // it is html
-        done();
-      });
-    });
-  });
-
-
-  describe('should be able to catch error', function() {
-    it('with callback api', function(done) {
-      this.timeout(10000);
-
-      fixture('https://gitfsdfsdfm', function(err, res) {
-        // callback api
-        assert.ok(err instanceof Error);
-        assert.strictEqual(res, null || undefined);
-        done();
-      });
-    });
-
-    it('with promise api - with .catch(err)', function(done) {
-      this.timeout(10000);
-
-      fixture('https://gitfsdfsdfm')
-      .catch(function(err) {
-        // promise api
-        assert.ok(err instanceof Error);
-        done();
-      });
-    });
-
-    it('with both callback and promise api', function(done) {
-      this.timeout(10000);
-
-      fixture('https://gitfsdfsdfm', function(err) {
-        // callback api
-        assert.ok(err instanceof Error);
-      })
-      .catch(function(err) {
-        // promise api
-        assert.ok(err instanceof Error);
-        done();
-      });
-    });
-  });
-
-  describe('should throw error', function() {
-    it('when first argument not a Promise', function(done) {
-      function iifn() {
-        handleCallback('str', function() {});
-      }
-      assert.throws(iifn, TypeError);
-      done();
-    });
-
-    it('when second argument not a Function', function(done) {
-      function iifn() {
-        var promise = thenGot.get('https://github.com');
-        handleCallback(promise, 'not a function');
-      }
-      assert.throws(iifn, TypeError);
-      done();
-    });
-  });
-});
+      test.ok(body.indexOf('<title>') !== -1)
+      test.equal(marker, 32)
+      done()
+    })
+  })
+  test('should handle errors in when both promise and callback', function (done) {
+    var marker = 11
+    fixture('random://github.com', function (err, res) {
+      test.ifError(!err)
+      test.ok(/Protocol.*not supported/.test(err.message))
+      test.equal(res, undefined)
+      marker += 22
+    })
+    .catch(function (err) {
+      test.ifError(!err)
+      test.ok(/Protocol.*not supported/.test(err.message))
+      test.equal(marker, 33)
+      done()
+    })
+  })
+})
